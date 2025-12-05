dd68c21ab4de2ea688336bd0ad79a3e0637a9508
Jordan Robinson <robinson-jordan@outlook.com>
2025-12-05 15:09:26 -0700

    Add basic character movement and mouse look functionality

---
+++ first_person_controller/scripts/look_left_right.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+# Use mouse motion to look left and right
+
+var look_sensitivity: float = 0.002
+var look_rotation: float = 0.0
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		# Apply vertical mouse motion to look_rotation
+		look_rotation -= event.screen_relative.x * look_sensitivity
+		
+		# Wrap between 0.0 and TAU (0 and 360 deg)
+		look_rotation = fposmod(look_rotation, TAU)
+		
+		# Reset basis to prevent precision errors due to successive operations
+		transform.basis = Basis()
+		
+		# Rotate left and right
+		rotate_object_local(Vector3(0.0, 1.0, 0.0), look_rotation)
---
+++ first_person_controller/scripts/look_up_down.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+# Use mouse motion to look up and down
+
+var look_sensitivity: float = 0.002
+var look_down_limit: float = -90.0
+var look_up_limit: float = 90.0
+var look_rotation: float = 0.0
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		# Apply horizontal mouse motion to look_rotation
+		look_rotation -= event.screen_relative.y * look_sensitivity
+		look_rotation = clampf(look_rotation, deg_to_rad(look_down_limit), deg_to_rad(look_up_limit))
+		
+		# Reset basis to prevent precision errors due to successive operations
+		transform.basis = Basis()
+		
+		# Rotate up and down
+		rotate_object_local(Vector3(1.0, 0.0, 0.0), look_rotation)
