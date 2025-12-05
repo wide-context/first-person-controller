e91af851f1b781084e8500ab54ca1517c9d6213e
Jordan Robinson <robinson-jordan@outlook.com>
2025-12-05 15:39:28 -0700

    Refactor first person controller script for readability

--- first_person_controller/scripts/first_person_controller.gd
+++ first_person_controller/scripts/first_person_controller.gd
@@ -38,13 +38,13 @@ func _physics_process(delta: float) -> void:
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right",
 			"move_forward", "move_backward")
 	var direction: Vector3 = (actor.global_transform.basis *
-			Vector3(input_dir.x, 0, input_dir.y)).normalized()
+			Vector3(input_dir.x, 0.0, input_dir.y)).normalized()
 	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0.0, SPEED)
+		velocity.z = move_toward(velocity.z, 0.0, SPEED)
 
 	move_and_slide()
