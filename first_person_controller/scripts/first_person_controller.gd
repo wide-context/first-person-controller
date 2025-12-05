b01075ecec6371269a33d1a4aff6d641a17f49b0
Jordan Robinson <robinson-jordan@outlook.com>
2025-12-05 15:35:57 -0700

    Add wasd and spacebar movement input actions

--- first_person_controller/scripts/first_person_controller.gd
+++ first_person_controller/scripts/first_person_controller.gd
@@ -25,18 +25,18 @@ func _process(_delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration
 	# Use the actor global basis to determine forward direction
-	var input_dir: Vector2 = Input.get_vector("ui_left", "ui_right",
-			"ui_up", "ui_down")
+	var input_dir: Vector2 = Input.get_vector("move_left", "move_right",
+			"move_forward", "move_backward")
 	var direction: Vector3 = (actor.global_transform.basis *
 			Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
